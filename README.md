57a5e3ec7ab5485a299df85dde429a47a603fb1b
Steven Martin <stevenisekimartin@gmail.com>
2015-03-20 00:17:39 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## browser version
+A node module for detecting the browser version.
+
+``` js
+var browserVersion = require('browser-version');
+console.log(browserVersion());
+
+```
---
+++ test/tests.js
@@ -0,0 +1,8 @@
+var test = require('tape');
+var browserVersion = require('../browser-version.js');
+
+test('version is chrome', function (t) {
+    t.equal(browserVersion(), "Chrome 39");
+		t.equal(2 + 3, 5);
+    t.end();
+});
