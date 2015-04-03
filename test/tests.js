3eb82c18da41f96e511d21eaecefeda26ce3c6bc
Steve Lacy <me@slacy.me>
2015-04-03 15:10:36 -0700

    lint code indentation and style

--- test/tests.js
+++ test/tests.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var browserVersion = require('../browser-version.js');
 
 test('version is chrome', function (t) {
-    t.equal(browserVersion(), "Chrome 39");
-		t.equal(2 + 3, 5);
-    t.end();
+  t.equal(browserVersion(), "Chrome 39");
+	t.equal(2 + 3, 5);
+  t.end();
 });
